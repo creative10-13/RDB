7a42e3c77d65b9ecc69ab13a3f4a2ccacbbe5f8a
creative10-13 <76442558+creative10-13@users.noreply.github.com>
2021-12-16 12:28:25 -0500

    Delete ping.js

--- ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'ping',
-    descrption: "basic ping command",
-    execute(message, args){
-        console.log("ping command was messaged");
-        message.channel.send('Pong!');
-    }
-}
